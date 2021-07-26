9717f98a3371c4299aa57f48a94b10ecc99432ab
HenPatis <>
2021-07-26 14:13:50 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,123 @@
+//class Main {
+  //public static void main(String[] args) {
+    //System.out.println("Hello world!");
+    //int num1;
+    //num1 = 3;
+    //int num2 = 4;
+    //int num3 = num1 + num2;
+    //System.out.println(num3);
+
+  //}
+//}
+
+
+//class Main {
+  //public static void main(String[] args) {
+    //int num56;
+    //num56 = 56+34-28;
+    //System.out.println(num56);
+  //}
+//}
+
+
+//class Main {
+  //public static void main(String[] args) {
+    //int num396;
+    //num396 = 3968+4868-49%3968;
+    //int num10;
+    //int num11;
+    //num10 = 43969475;
+    //num11 = num396 + num10;
+    //System.out.println(num11);
+  //}
+//}
+
+
+//class Main {
+  //public static void main(String[] args) {
+    //String word = "one ";
+    //String word2 = "two";
+    //System.out.println(word + word2);
+  //}
+//}
+
+
+//class Main {
+  //public static void main(String[] args) {
+    /*System.out.println("Hello world!");
+    int num1;
+    //הצהרה של הערך int
+    num1 = 3;
+    //השמה של הערך int
+    int num2 = 4;
+    //הצהרה והשמה של הערך int
+    int num3 = num1 + num2;
+    System.out.println(num3);
+    int num4 = 456;
+    int num5 = 789;
+    int num6 = num4*num5*num3;
+    System.out.println(num6*8);
+    System.out.println(num3 - num4 - num5);
+    //פעולות במשתנים של הערך int
+    String s = "Hello";
+    //הצהרה והשמה של הערך String
+    System.out.println(s);
+    //הדפסה של הערך String
+    */
+  //}
+//}
+
+
+//class Main {
+  //public static void main(String[] args) {
+    //String word = "Hen";
+
+    //int age;
+    //age = 12;
+
+    //String word2 = "My name is ";
+    //String word3 = " and my age is ";
+
+    //System.out.println(word2+word+word3+age);
+  //}
+//}
+
+//import java.util.*;
+//class Main {
+  //public static void main(String[] args) {
+    //Scanner input = new Scanner(System.in);
+    //int x = input.nextInt();
+    //System.out.println("your age is" + x);
+    //int num1;
+    //num1 = 5;
+    //int num2 = 7;
+    //int num3 = num1 + num2;
+    //System.out.println(num3);
+    //String word1 = "hi ";
+    //String word2 = "word";
+    //System.out.println(word1 + word2);
+
+  //}
+//}
+
+//import java.util.*;
+//class Main {
+  //public static void main(String[] args) {
+   //Scanner input = new Scanner(System.in);
+    //System.out.println("enter your age:");
+    //int x = input.nextInt();
+    //System.out.println("your age is " + x);
+    //System.out.println("enter your neime:");
+    //String b = input.next();
+    //System.out.println("your neime is " + b);
+  //}
+//}
+
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+   Random rnd = new Random();
+    int r = rnd.nextInt(10);
+    System.out.println(r);
+  }
+}
